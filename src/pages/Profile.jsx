488e26bc27dc52b2c9a1420c226e809ecc2638a9
kl2400030612 <2400030612@kluniversity.in>
2025-11-29 00:10:04 +0530

    Profile

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+export default function Profile() {
+  return (
+    <Layout>
+      <h2>Profile</h2>
+      <p>Name: John Student</p>
+      <p>Email: student@example.com</p>
+    </Layout>
+  );
+}
