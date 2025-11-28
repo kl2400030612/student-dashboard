62680296efc471952ea2ba50c686348aa135edbe
kl2400030612 <2400030612@kluniversity.in>
2025-11-29 00:08:35 +0530

    Topbar

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,10 @@
+import "./Topbar.css";
+
+export default function Topbar() {
+  return (
+    <header className="topbar">
+      <h3>Welcome Student</h3>
+      <div className="profile">Student</div>
+    </header>
+  );
+}
